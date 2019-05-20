1783e33a096bb2f7f83a55098f4babdef54b68e7
DESKTOP-1AEU2HP\hoon <pung8146@naver.com>
2019-05-20 21:56:27 +0900

    0520 document 와 getElementById("title");
    title.innerHTML = "바꿀내용";

---
+++ vanilia_js/index2.js
@@ -0,0 +1,9 @@
+// 아래랑 같음 console.log(document.getElementById);
+
+const title = document.getElementById("title");
+
+console.log(title);
+
+console.error("fuck")
+
+title.innerHTML = "Hi! From JS";
