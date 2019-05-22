340bfdb333677fae4382455d8b1efee59da59632
DESKTOP-1AEU2HP\hoon <pung8146@naver.com>
2019-05-22 22:41:55 +0900

    0522
    자바스크립트로 문서의 내부내용을 바꿀수 있으며  그외
    홈페이지 타이틀 등 도 수정이 가능하다.
    
    querySelector 을 사용할때는 " #원하는id" or " .원하는 클래스"  앞에 표시해주어야함 !!

--- vanilia_js/index2.js
+++ vanilia_js/index2.js
@@ -3,7 +3,12 @@
 const title = document.getElementById("title");
 
 console.log(title);
-
+console.dir(title);
 console.error("fuck")
 
 title.innerHTML = "Hi! From JS";
+title.style.color = "red";
+document.title = "I own you now" 
+
+// quertSelector 는 노드의 첫번째 자식을 반환
+// 의미는 ("#원하는아이디")or(".class") # or .이 무조건 붙어있어야함 !!
