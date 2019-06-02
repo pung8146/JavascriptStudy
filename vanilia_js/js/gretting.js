ec14a2bd4f368dbb61a09d8485c8df0a7dd3366e
pung8146 <red8146@gmail.com>
2019-06-03 08:55:40 +0900

    3_3&3_4 강좌에서 배운것         1.각 js는 각각 나눠주는것이 좋을수도있다         2.style css 부를때 상수로 지정하고 .필요없이 사용하면되는거같음???(추가공부필요)         3.로컬스토리지에서 저장은 다른홈페이지 이동해도 남아있는다.         4.event.preventDefault()로 기본이벤트를 막아줌으로써 새로고침 되는효과를 막아줌 기본시작         5.classList에 remove.add로 추가 시키고 없앨수있음         6.그것을 === !== 등 이프문에서도 가능         최종결론 : 이번 강좌는 로컬스토리지에 대한 개념을 이해하기에 좋았지만                    추가적으로 여러가지의 상수와 호출개념이 사용되어                    약간 햇갈리도함 그렇기에 내가 혼자 만들어내기에는 아직 어려운거같음                    다시 복습이필요 할 꺼 같다

--- vanilia_js/js/gretting.js
+++ vanilia_js/js/gretting.js
@@ -1,20 +1,37 @@
 const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
-    greeting = document.querySelector(".js-greeting");
+    greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
-function paintGreeting(text){
-        form.classList.remove(SHOWING_CN);
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit" , handleSubmit)
+}
+
+function paintGreeting(text) {
+    form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerTExt = `Hello ${text}`;
+    greeting.innerText = `Hello ${text}`;
 }
 
 function loadName(){
-    const curentUser = localStorage.getItem(USER_LS)
+    const currentUser = localStorage.getItem(USER_LS)
     if(currentUser === null){
         // she is not
+        askForName();
     }else{
         // she is
         paintGreeting(currentUser);
