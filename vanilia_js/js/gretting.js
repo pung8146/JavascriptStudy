dcf878a4b2d9ea3f945f4d53cfba87a328a02108
pung8146 <red8146@gmail.com>
2019-06-02 16:58:29 +0900

    3_1 강좌에서 배운것         1., 로 const 중복을 생략가능 할 수 있음         2. const사용때 const 소문자 = new 대문자();         3. init (); 초기화 시키고 사용했으며         4. innerHTML로 html에서 텍스트로 표현했음         5. `` {비택 사용법 다시 복습}         6. date 은 함수         7. 만약에 date으로 한번의 시간대 함수를 저장했으면 계속 그시간대의 분이 불려짐     3_2 강좌에서 배운것         1.setInterval(인자,1000) 인자로불려진 함수를 정해논 시간 마다 실행         2.미니 if ( <  or > ) ? : 순으로 사용         2-1.ex 10이하일때 실행시킬때 ( ex < 10 ? `${함수}` : 함수)             10이하 이면 첫번째를 시키고 아니면 뒤에꺼를 실행시켜라

---
+++ vanilia_js/js/gretting.js
@@ -0,0 +1,28 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greeting");
+
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
+
+function paintGreeting(text){
+        form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerTExt = `Hello ${text}`;
+}
+
+function loadName(){
+    const curentUser = localStorage.getItem(USER_LS)
+    if(currentUser === null){
+        // she is not
+    }else{
+        // she is
+        paintGreeting(currentUser);
+    }
+}
+
+function init(){
+    loadName();
+}
+
+init();
